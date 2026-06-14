4b6ed4fdf0c71e829b52bb9f66fc5fe7764aa58d
sambhavjohari <sambhavjohari15@gmail.com>
2026-06-14 08:27:58 +0530

    Add role skills mapping for various job titles

---
+++ skills_db.py
@@ -0,0 +1,38 @@
+ROLE_SKILLS = {
+
+    "Data Analyst": [
+        "Excel",
+        "SQL",
+        "Python",
+        "Power BI",
+        "Statistics",
+        "Tableau"
+    ],
+
+    "AI Engineer": [
+        "Python",
+        "Machine Learning",
+        "Deep Learning",
+        "TensorFlow",
+        "NLP",
+        "Data Science"
+    ],
+
+    "Cloud Engineer": [
+        "AWS",
+        "Azure",
+        "Docker",
+        "Linux",
+        "Kubernetes",
+        "Networking"
+    ],
+
+    "Software Developer": [
+        "C++",
+        "Java",
+        "Python",
+        "Git",
+        "DBMS",
+        "Data Structures"
+    ]
+}
